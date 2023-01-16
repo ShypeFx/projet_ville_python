d974882bc26d9f4f157150c96422bb2d917ef9da
Tristan BEAUDOIN <tristan.beaudoin@gmail.com>
2023-01-16 14:36:15 +0100

    searching city & get the closer ones

--- project_main.py
+++ project_main.py
@@ -36,4 +36,46 @@ def qick_distance(coord1, coord2):
     return 111.319 * sqrt(x*x + y*y)
 
 
-read_from_file()
+def find_coord_city_name(city):
+    with open('./laposte_hexasmal_mod.csv') as orders_file:
+        spamreader = csv.reader(orders_file, delimiter=';')
+        for row in spamreader:
+            list_of_strings = row[5].split(',')
+            if(row[1].__eq__(city)):
+                #print('type : ', type(row[5]))
+                #print('ville : ', city)
+                #print('coordonnee : ',row[5])
+                #print('codepostale : ', row[2])
+                list_of_strings = row[5].split(',')
+                list_float = [float(x) for x in list_of_strings]
+                return list_float
+
+
+def information_city(coordonates):
+    with open('./laposte_hexasmal_mod.csv') as orders_file:
+        spamreader = csv.reader(orders_file, delimiter=';')
+        for row in spamreader:
+            if(row[5].__eq__(coordonates)):
+                print('ville : ', row[1])
+                print('codepostale : ', row[2])
+                print('coordonnee : ', coordonates)
+def min_from_city(coordonates):
+    with open('./laposte_hexasmal_mod.csv') as orders_file:
+        spamreader = csv.reader(orders_file, delimiter=';')
+        for row in spamreader:
+            if row[5] != '':
+                list_of_strings = row[5].split(',')
+                list_float = [float(x) for x in list_of_strings]
+                res = qick_distance(coordonates,list_float)
+                if(res < 10):
+                    print(' ville : ',row[1])
+                    print(' distance : ', res)
+
+
+
+def main():
+    city = input(" ENTRER UNE VILLE : ")
+    coordonate_var = find_coord_city_name(city)
+    min_from_city(coordonate_var)
+
+main()
